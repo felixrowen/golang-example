2877741688034526ef85f25fd5ae9c290fd125b9
felixrowen <felixroo98@gmail.com>
2019-12-27 03:35:32 +0700

    add 03_packages

--- 03_packages/main.go
+++ 03_packages/main.go
@@ -3,8 +3,13 @@ package main
 import (
 	"fmt"
 	"math"
+
+	"github.com/felixrowen/go_tutorial/03_packages/strutil"
 )
 
 func main() {
-	fmt.Println(math.Floor(2.6))
+	fmt.Println(math.Floor(2.3))
+	fmt.Println(math.Ceil(2.7))
+	fmt.Println(math.Sqrt(16))
+	fmt.Println(strutil.Reverse("olleh"))
 }
